09a9f14b2f73f12931b9075f01495902b6dff6ef
DenDiva Reva Rafliansya <divareva55@gmail.com>
2025-10-05 20:40:04 +0700

    Update

--- lib/features/wallet/logic/wallet_state.dart
+++ lib/features/wallet/logic/wallet_state.dart
@@ -1,16 +1,13 @@
 part of 'wallet_bloc.dart';
 
-// Abstract base class untuk semua state
 abstract class WalletState extends Equatable {
   const WalletState();
   @override
   List<Object> get props => [];
 }
 
-// State awal saat data belum dimuat
 class WalletInitial extends WalletState {}
 
-// State ketika data saldo dan poin sudah berhasil dimuat
 class WalletLoaded extends WalletState {
   final double balance;
   final int points;
