f676f4899651247ea035d7179a0cce5305444fa3
DenDiva Reva Rafliansya <divareva55@gmail.com>
2025-10-05 20:36:45 +0700

    Update

--- lib/features/wallet/data/repositories/wallet_repository.dart
+++ lib/features/wallet/data/repositories/wallet_repository.dart
@@ -1,12 +1,9 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class WalletRepository {
-  // Kita tidak lagi menggunakan key statis di sini
 
-  // Memuat data saldo dan poin untuk pengguna spesifik
   Future<Map<String, num>> loadWallet(String userEmail) async {
     final prefs = await SharedPreferences.getInstance();
-    // Buat kunci yang unik untuk setiap pengguna
     final balanceKey = '${userEmail}_balance';
     final pointsKey = '${userEmail}_points';
     
@@ -15,14 +12,12 @@ class WalletRepository {
     return {'balance': balance, 'points': points};
   }
 
-  // Menyimpan data saldo dan poin untuk pengguna spesifik
   Future<void> saveWallet({
     required String userEmail,
     required double balance,
     required int points,
   }) async {
     final prefs = await SharedPreferences.getInstance();
-    // Buat kunci yang unik untuk setiap pengguna
     final balanceKey = '${userEmail}_balance';
     final pointsKey = '${userEmail}_points';
 
